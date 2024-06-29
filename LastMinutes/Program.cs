378814211eb5ee1e6ff1faa4406d52671b4456f5
Luke Kearney <mrdawggames@gmail.com>
2024-06-29 09:37:46 +0200

    Updated app version

--- LastMinutes/Program.cs
+++ LastMinutes/Program.cs
@@ -53,7 +53,7 @@ if (!ConfigPass)
  *  -tag = Used if multiple releases are made per day (example: -a, -b, -test)
  */
 
-string AppVersion = "10318";
+string AppVersion = "10628";
 string AppStage = "Beta";
 
 configuration.AddInMemoryCollection(new Dictionary<string, string>
